62d140806b2a61c5b30880481ab76f65f226e7ad
Shavv66 <25536624+Shavv66@users.noreply.github.com>
2017-02-04 03:13:32 +0000

    Created solution with application and engine projects

---
+++ src/SimulationEngine/SimulationApplicationServices/SimulationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimulationApplicationServices
+{
+	public class SimulationService
+    {
+		public void RunSimulation()
+		{
+			Console.WriteLine("Such simulate");
+		}
+    }
+}
---
+++ src/SummonersWarSimulator/Program.cs
@@ -0,0 +1,13 @@
+﻿using SimulationApplicationServices;
+
+namespace SummonersWarSimulator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SimulationService simulationService = new SimulationService();
+			simulationService.RunSimulation();
+		}
+	}
+}
